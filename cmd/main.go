6496061e23a6fd012d22b1e52d2fda75f9105385
hiro7392 <hiromiimkw@gmail.com>
2025-02-12 23:07:11 +0900

    fix proto

--- cmd/main.go
+++ cmd/main.go
@@ -3,31 +3,37 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 
 	"github.com/hiro7392/get-business-calendar/internal/handler"                // サービス実装があるパッケージ
 	pb "github.com/hiro7392/get-business-calendar/internal/proto/business_days" // proto 定義のパッケージ
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":9090")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
+	port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    lis, err := net.Listen("tcp", ":"+port)
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
+	
 	grpcServer := grpc.NewServer()
 
-	// handler パッケージ内の実装で BusinessDays サービスのサーバーインスタンスを生成
-	businessDaysServer := handler.NewBusinessDaysServer()
+	healthServer := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(grpcServer, healthServer)
 
-	// 生成された proto のコードを利用して、gRPC サーバーにサービスを登録
+	businessDaysServer := handler.NewBusinessDaysServer()
 	pb.RegisterBusinessDaysServer(grpcServer, businessDaysServer)
 
 	reflection.Register(grpcServer)
 
-	log.Println("Server is running on port :9090")
-	// サーバーの起動
+	log.Printf("Server is running on port :%s",port)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
